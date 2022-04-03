9f1ae4374b0f6a11e369bab1f9a0abca37068d36
Pratap689 <kushwahapratap11@gmail.com>
2022-04-03 13:40:43 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Pratap Rana on 03/04/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
